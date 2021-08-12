4a87d52a1378c740476d2e55b91d7f6673ffdaf6
taylor herbots <therbots@live.com>
2021-08-11 20:12:10 -0500

    changed damage and health values for dinos to try to balance it out

--- herd.py
+++ herd.py
@@ -8,9 +8,9 @@ from dinosaur import Dinosaur
 class Herd:
     def __init__(self):
         self.herd = []
-        self.dinosaur_1 = Dinosaur('Velociraptor', 15, 100)
+        self.dinosaur_1 = Dinosaur('Velociraptor', 20, 100)
         self.dinosaur_2 = Dinosaur('T-Rex', 35, 150)
-        self.dinosaur_3 = Dinosaur('Stegosaurus', 15, 200)
+        self.dinosaur_3 = Dinosaur('Stegosaurus', 15, 250)
 
     def create_herd(self):
         self.herd.append(self.dinosaur)
